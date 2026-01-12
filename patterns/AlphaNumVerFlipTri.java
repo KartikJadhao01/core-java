a4e511fda2ff10929023d0fa4f806e54eb6a1384
KartikJadhao01 <143504998+KartikJadhao01@users.noreply.github.com>
2026-01-12 23:21:33 +0530

    Add files via upload

---
+++ patterns/AlphaNumVerFlipTri.java
@@ -0,0 +1,27 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class AlphaNumVerFlipTri {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for (int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                if (i%2==0){
+                    System.out.print((char) (j+64)) ;
+                }
+                else {
+                    System.out.print(j);
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
