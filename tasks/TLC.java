6504e611e41988f0365cde3f40438971f8cc6cfa
KartikJadhao01 <143504998+KartikJadhao01@users.noreply.github.com>
2026-01-18 11:57:43 +0530

    Add files via upload

---
+++ tasks/TLC.java
@@ -0,0 +1,19 @@
+package task;
+
+public class TLC extends Thread {
+	public void run() {
+        System.out.println("Run");
+    }
+    public static void main(String[] args) {
+        TLC t = new TLC();
+        System.out.println("Thread name: " + t.getName());
+        System.out.println("Thread state before start: " + t.getState());
+        t.start();
+        System.out.println("Thread state after start: " + t.getState());
+//        t.sleep(1000);
+        t.run();
+        System.out.println("Thread state: " + t.getState());
+        System.out.println("Thread status: " + t.isAlive());
+
+    }
+}
