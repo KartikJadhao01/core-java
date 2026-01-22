e4b8a5224a59521e2078623a3efcf7caa0f2786b
KartikJadhao01 <143504998+KartikJadhao01@users.noreply.github.com>
2026-01-22 21:27:24 +0530

    Add files via upload

---
+++ practical/replace_1.java
@@ -0,0 +1,12 @@
+package practical;
+
+public class replace_1 {
+	public static void main(String[] args) {
+		String name = "P4r8i6ya 3Ra1u9t";
+		String new_name = name.replaceAll("[0-9]", " ");
+		
+		System.out.println(new_name);
+	}
+
+
+}
