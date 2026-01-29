adb445fba218f39606a7f3c30cc9624faecf851b
KartikJadhao01 <143504998+KartikJadhao01@users.noreply.github.com>
2026-01-29 11:59:34 +0530

    Add files via upload

---
+++ collectionFramework/DeQueueDemo.java
@@ -0,0 +1,22 @@
+package collectionFramework;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class DeQueueDemo {
+
+	static void dque() {
+		ArrayDeque q = new ArrayDeque();
+		
+		q.add(1);
+		q.add("Kartik");
+		
+		System.out.println(q);
+	}
+	
+	public static void main(String[] args) {
+		dque();
+	}
+}
+ 	
