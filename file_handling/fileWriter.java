1f8b32067057a919e994481be5b3f49dddca8eca
KartikJadhao01 <143504998+KartikJadhao01@users.noreply.github.com>
2026-02-06 23:20:43 +0530

    Update fileWriter.java

--- file_handling/fileWriter.java
+++ file_handling/fileWriter.java
@@ -3,7 +3,7 @@ package file_handling;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class fileWriter {
+public class fileWriter{
 public static void main(String[] args) throws IOException {
 	
 	FileWriter fw=new FileWriter("C:\\Users\\Kartik\\filewrite.txt");
